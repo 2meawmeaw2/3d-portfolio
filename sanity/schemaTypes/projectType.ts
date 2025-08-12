35514743f8898a4dea012a7a1f64181e28d2f106
2meawmeaw2 <tahaslco@gmail.com>
2025-08-12 10:35:02 +0100

    dark/light theme removed

---
+++ sanity/schemaTypes/projectType.ts
@@ -0,0 +1,27 @@
+export const projectType = {
+  name: "project",
+  type: "document",
+  title: "Project",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Project Title",
+    },
+    {
+      name: "description",
+      type: "text",
+      title: "Description",
+    },
+    {
+      name: "url",
+      type: "url",
+      title: "Project URL",
+    },
+    {
+      name: "image",
+      type: "image",
+      title: "Project image",
+    },
+  ],
+};
